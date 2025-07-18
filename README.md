68a0b184eb8b32b65ff8125e72745826851ac6ed
Anirudh Saksena <anen08saks@gmail.com>
2025-07-18 14:36:33 +0530

    Made a simple plot for plotting numbers and their using basic pyplot functionalities

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Data-Visualization
+
+This readme will be updated soon :D
---
+++ mpl_squares.py
@@ -0,0 +1,32 @@
+"""
+- Most of the matplots functionality is in pyplot
+- squares is a list holding the actual data
+- fig is the entire canvas on which plots will be displayed
+- ax is an axes/subplot that will be displayed on the fig canvas
+- subplots() allows us to generate one or more plots in the same collection of plots generated. It allows you generate mutiple plots on same canvas
+- plot() tries to plot the data it is given
+"""
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.use("TkAgg")
+
+input_values = list(range(1, 6))
+squares = [value**2 for value in range(1, 6)]
+
+# Style
+plt.style.use("fivethirtyeight")
+
+fig, ax = plt.subplots()
+ax.plot(input_values, squares, linewidth=3)
+
+# Set chart title and label axes
+ax.set_title("Sqaure Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of the value", fontsize=14)
+
+# Set the size of the tick labels
+ax.tick_params(labelsize=14)
+
+plt.show()
