f7b6081fb75b8f81aa8cd78cced7de156ce24b0b
Anirudh Saksena <anen08saks@gmail.com>
2025-07-22 17:26:42 +0530

    Plotly basics

---
+++ plotly/die_visual.py
@@ -0,0 +1,28 @@
+from die import Die
+import plotly.express as px
+
+# Create a die
+die1 = Die()
+die2 = Die()
+# Make some rolls, and store results in a list
+
+results = []
+for roll_num in range(1000):
+    result = die1.roll() + die2.roll()
+    results.append(result)
+
+# Analyze the result
+frequencies = []
+max_result = die1.num_sides + die2.num_sides
+poss_results = range(2, max_result + 1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
+
+title = "Result of Rolling two D6 1000 times"
+labels = {"x": "Result", "y": "Frequency of Result"}
+# Visualize the results
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+fig.show()
