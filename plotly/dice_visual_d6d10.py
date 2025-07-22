574e3d92792f7c432eab1161685e2a02d5a5e122
Anirudh Saksena <anen08saks@gmail.com>
2025-07-22 17:45:42 +0530

    Added label customization to die_visual and dice_visual_d6d10 modules

--- plotly/dice_visual_d6d10.py
+++ plotly/dice_visual_d6d10.py
@@ -1,27 +1,28 @@
 from die import Die
 import plotly.express as px
 
-# Create a die
-die1 = Die()
-die2 = Die()
-# Make some rolls, and store results in a list
+# Create a D6 and a D10
 
+die_1 = Die()
+die_2 = Die(10)
+
+# Make some rolls and store the results
 results = []
-for roll_num in range(1000):
-    result = die1.roll() + die2.roll()
+for roll_num in range(50_000):
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
-# Analyze the result
+# Analyze the results
 frequencies = []
-max_result = die1.num_sides + die2.num_sides
-poss_results = range(2, max_result + 1)
+max_results = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_results + 1)
+
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
 
-print(frequencies)
 
-title = "Result of Rolling two D6 1000 times"
+title = "Result of Rolling two D6 and D10 50,000 times"
 labels = {"x": "Result", "y": "Frequency of Result"}
 # Visualize the results
 fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
