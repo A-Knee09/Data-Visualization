f72df0af8dfa9bfd922f1b6ea5c886a885e395d7
Anirudh Saksena <anen08saks@gmail.com>
2025-07-22 13:05:06 +0530

    Refactored the random walk genration module

--- basics/random_walk.py
+++ basics/random_walk.py
@@ -12,21 +12,19 @@ class RandomWalk:
         self.x_values = [0]
         self.y_values = [0]
 
+    def get_step(self, axis="x"):
+        """Generate a step in a direction"""
+        direction = choice([-1, 1])
+        distance = choice([1, 2, 3, 4]) if axis == "x" else choice([0, 1, 2, 3, 4])
+        return direction * distance
+
     def fill_walk(self):
         """Calculate all the points of the walk"""
 
         # Keep taking the steps until walk reaches desired length
-
         while len(self.x_values) < self.num_points:
-
-            # Decide which direction to go, and how far to go
-            x_direction = choice([1, -1])
-            x_distance = choice([1, 2, 3, 4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = self.get_step("x")
+            y_step = self.get_step("y")
 
             # Rejecting moves that go nowhere
             if x_step == 0 and y_step == 0:
